04c8772813e75e38b90a8a2174f36ecaa0713f29
Arif Sulaiman <arif.sulaiman720@gmail.com>
2022-01-23 12:54:32 +0700

    site

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,30 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'Web Warrior',
+    description: 'web dev portfolio',
+    copyright: 'This website is copyright 2022 web warrior',
+    contact: 'me@thewebwarriorid.com'
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,34 @@
-import React from "react"
+import { graphql, Link } from 'gatsby';
+import React from 'react';
+import Layout from '../components/Layout';
+import { header, btn } from '../styles/home.module.css';
+import { GatsbyImage } from 'gatsby-plugin-image';
+;
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+export default function Home({ data }) {
+  console.log(data);
+  return (
+    <Layout>
+      <section className={header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Manchester.</p>
+          <Link className={btn} to='/projects'>My Portfolio Projects</Link>
+        </div>
+        <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} alt='Banner image'></GatsbyImage>
+      </section>
+    </Layout>
+  );
 }
+
+export const query = graphql`
+query Banner {
+  file(relativePath: {eq: "banner.png"}) {
+    childImageSharp {
+      gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+    }
+  }
+}
+`
