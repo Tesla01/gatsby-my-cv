d59ebc754a8f79809e602c79342cd5a5d3803565
Arif Sulaiman <arif.sulaiman720@gmail.com>
2022-01-23 13:00:06 +0700

    Change homepage text

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ data }) {
     <Layout>
       <section className={header}>
         <div>
-          <h2>Design</h2>
+          <h2>New Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Manchester.</p>
           <Link className={btn} to='/projects'>My Portfolio Projects</Link>
